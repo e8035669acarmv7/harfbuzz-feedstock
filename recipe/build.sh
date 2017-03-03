8d7890ca3acacfb682e1ca5afdbb5b687305a747
Peter Williams <peter@newton.cx>
2017-03-03 09:06:01 -0500

    Update to version 1.4.3
    This comes with some general tidying of the build script. We also add
    graphite2 as a dependency and produce GObject introspection bindings on both
    Linux and OSX.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 
+set -e
+
 if [ $(uname) == Darwin ]; then
   export CC=clang
   export CXX=clang++
   export MACOSX_DEPLOYMENT_TARGET="10.9"
   export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
-  export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
-  export OPTS=""
-elif [[ $(uname) == Linux ]]; then
-  export OPTS="--with-gobject"
 fi
 
-# We do not need this when building locally. Weird.
-autoreconf --force --install
+# CircleCI seems to have some weird issue with harfbuzz tarballs. The files
+# come out with modification times such that the build scripts want to rerun
+# automake, etc.; we need to run it ourselves since we don't have the precise
+# version that the build scripts embed. And the 'configure' script comes out
+# without its execute bit set. In a Docker container running locally, these
+# problems don't occur.
 
-# FIXME: Locally it does have the executable bits,
-# but for some reason it does not work on CircleCi :-/
-bash configure --prefix=$PREFIX \
-               --disable-gtk-doc \
-               --enable-static \
-               $OPTS
+autoreconf --force --install
+chmod +x configure
+
+./configure --prefix=$PREFIX \
+            --disable-gtk-doc \
+            --enable-static \
+            --with-graphite2=yes \
+            --with-gobject=yes
 
 make
-# FIXME
-# OS X:
-# FAIL: test-ot-tag
-# Linux (all the tests pass when using the docker image :-/)
-# FAIL: check-c-linkage-decls.sh
-# FAIL: check-defs.sh
-# FAIL: check-header-guards.sh
-# FAIL: check-includes.sh
-# FAIL: check-libstdc++.sh
-# FAIL: check-static-inits.sh
-# FAIL: check-symbols.sh
-# PASS: test-ot-tag
-# make check
+make check
 make install
+
+pushd $PREFIX
+rm -rf share/gtk-doc
+popd
