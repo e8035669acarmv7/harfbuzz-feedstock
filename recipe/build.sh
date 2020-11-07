41decd64921296ea5e8fe638dc740dc1b9ffe44e
Isuru Fernando <isuruf@gmail.com>
2020-11-07 02:42:29 -0600

    set PKG_CONFIG

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -ex
 # necessary to ensure the gobject-introspection-1.0 pkg-config file gets found
 # meson needs this to determine where the g-ir-scanner script is located
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
 
 meson setup builddir \
         ${MESON_ARGS} \
